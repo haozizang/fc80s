faf1b36fda983ba3535758a3f2e2076b5b837efc
hao.zhang <choukou197@gmail.com>
2020-12-22 00:24:20 +0900

    add: multiple views in an app

--- fc80s_back/index/urls.py
+++ fc80s_back/index/urls.py
@@ -3,6 +3,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # /index/
     path('', views.index, name='index'),
+    # /index/24/
+    path('<int:detail_id>', views.detail, name='detail'),
+    # /index/3/question/
+    path('<int:detail_id>/question/', views.question, name='question'),
+    # /index/6/result
+    path('<int:detail_id>/result/', views.result, name='result'),
 ]
 
--- fc80s_back/index/views.py
+++ fc80s_back/index/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Player
 
 def index(request):
     # turn hex kanji into string
@@ -10,4 +11,20 @@ def index(request):
     print("GET", request.GET)
     print("POST", request.POST)
     print("Method", request.method)
-    return HttpResponse("Hello, this is fc80s-back!")
+    # get all players
+    player_list = Player.objects.all();
+    all_player_str = ""
+    for player in player_list:
+        all_player_str += "name: " + player.name + "<br>"
+        all_player_str += "offence: " + str(player.offence) + "<br>"
+        all_player_str += "defence: " + str(player.defence) +  "<br>"
+    return HttpResponse(all_player_str)
+
+def detail(request, detail_id):
+    return HttpResponse("You're looking at detail %s." % detail_id)
+
+def question(request, detail_id):
+    return HttpResponse("You're looking at question %s." % detail_id)
+
+def result(request, detail_id):
+    return HttpResponse("You're looking at result %s." % detail_id)
