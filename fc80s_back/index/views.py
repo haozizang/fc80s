11bd97ab9879c4e9df2844ac555b99e07b417e45
hao.zhang <choukou197@gmail.com>
2021-02-04 00:07:56 +0900

    back: update index main logic

--- fc80s_back/index/views.py
+++ fc80s_back/index/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import json
 from django.http import HttpResponse, Http404
 from django.template import loader
 from django.db.models import Q
@@ -7,20 +8,43 @@ from .models import Player, Match, Team
 
 def index(request):
     # turn hex kanji into string
-    body = str(request.body, encoding = "utf8")
-    # body_kanji = body.decode("utf-8").encode("utf-8")
-    print("body", body)
+    body_str = str(request.body, encoding = "utf8")
+    body_unicode = request.body.decode("utf-8")
+    body = json.loads(body_unicode)
+    print("body", body_str)
     print("path", request.path)
-    print("GET", request.GET)
-    print("POST", request.POST)
     print("Method", request.method)
-    # get all players
-    player_list = Player.objects.all();
-    # loader is old-fashioned
-    context = {
-        'player_list': player_list
-    }
-    return render(request, "index/index.html", context)
+    # get or create the player for the sender
+    player, if_created = Player.objects.get_or_create(open_id=body["open_id"], defaults={'name': body["nick_name"], 'open_id': body["open_id"]})
+    # get all the team(activity) the sender get involved
+    team_list = Team.objects.filter(players__open_id=body["open_id"])
+    team_num = team_list.count()
+    print("team num(activity num): ", team_list.count())
+    # get all the matches the sender's in
+    match_num = 0
+    for team in team_list:
+        print('team name: ', team.name)
+        home_match_list = Match.objects.filter(home_team__id=team.id)
+        away_match_list = Match.objects.filter(away_team__id=team.id)
+        print("home match num: ", home_match_list.count())
+        print("away match num: ", away_match_list.count())
+        match_num += home_match_list.count()
+        match_num += away_match_list.count()
+        print("match count: ", match_num)
+    resp = {
+        'activities': team_num,
+        'matches': match_num,
+        'offence': player.offence,
+        'defence': player.defence,
+        'stability': player.stability,
+        'teamwork': player.teamwork,
+        'passion': player.passion,
+        'win_ratio': player.win_ratio
+        }
+    print("player.name: ", player.name)
+    print("if created: ", if_created)
+    # return render(request, "index/index.html", context)
+    return HttpResponse(json.dumps(resp), content_type="application/json")
 
 def match(request, match_id):
     try:
